cc9ec84b05559025891d63a11d6aa390812f4369
framgia <you@example.com>
2018-11-14 17:59:52 +0700

    readme and cart

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
 			['name' => 'admin', 'email' => 'admin@gmail.com', 'password' => Hash::make('12345678'), 'address' => 'Ha Noi', 'phone' => '0396136933' ,'id_role' => 1, 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'user', 'email' => 'user@gmail.com', 'password' => Hash::make('12345678'), 'address' => 'Ha Noi', 'phone' => '0636136933' ,'id_role' => 2, 'created_at' => new DateTime, 'updated_at' => new DateTime],
 		]);	
     }
 }
