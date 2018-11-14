141123234769302123a556cec604a782a5c6ca27
lienhoang <hoanglien1201@gmail.com>
2018-11-14 17:29:34 +0700

    dep roi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,12 @@
 @section('title','Login')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="login" >
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">
+                    <h2 class="text-center">{{ __('Login') }}</h2>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -40,26 +42,28 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
+                            <div class="check">
+                                <div class="form-check col-md-6">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
+
                                 </div>
+                                <a class="btn btn-link col-md-4" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="btn-submit-login">
+                                <button type="submit" class="btn btn-primary btn-success center">
                                     {{ __('Login') }}
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
+                                
                             </div>
                         </div>
                     </form>
